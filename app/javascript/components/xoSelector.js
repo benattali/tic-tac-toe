1415268c55bca738c053a63bdd34f848869a7c39
Ben Attali <ben.attali8@gmail.com>
2019-10-03 17:01:45 +0300

    JS

--- app/javascript/components/xoSelector.js
+++ app/javascript/components/xoSelector.js
@@ -1,4 +1,5 @@
 const xoSelector = () => {
+    const board = document.querySelectorAll('.square');
     window.addEventListener('click', function(event) {
         const square = event.target;
         square.innerText = 'X';
