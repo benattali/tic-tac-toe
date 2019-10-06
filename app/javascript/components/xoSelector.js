c0cf139a7ec84cd6214ccaefcc88f1582954ea2c
Ben <ben.attali8@gmail.com>
2019-10-06 18:20:38 +0300

    altering o and x

--- app/javascript/components/xoSelector.js
+++ app/javascript/components/xoSelector.js
@@ -1,8 +1,18 @@
 const xoSelector = () => {
     const board = document.querySelectorAll('.square');
+    let oChecker = false;
     window.addEventListener('click', function(event) {
         const square = event.target;
-        square.innerText = 'X';
+
+        if ((square.id === 'top-left') || (square.id === 'top-center') || (square.id === 'top-right') || (square.id === 'mid-left') || (square.id === 'mid-center') || (square.id === 'mid-right') ||
+            (square.id === 'bottom-left') || (square.id === 'bottom-center') || (square.id === 'bottom-right'))
+              if (oChecker === false) {
+                square.innerText = 'X';
+                oChecker = true;
+              } else {
+                square.innerText = 'O';
+                oChecker = false;
+              }
     });
 }
 
