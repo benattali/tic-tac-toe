d287181998d712548545662bfa47328126fba6c2
Ben <ben.attali8@gmail.com>
2019-10-21 13:22:22 +0300

    new game starts with x

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,19 +1,38 @@
 const main = () => {
   let oChecker = false;
+  let newGame = false;
 
-  window.addEventListener('click', function(event) {
+  // Event listener to check for a new game situation
+  window.addEventListener('mousedown', function(event) {
+    const board = document.querySelector('.main-board');
+    const children = board.children;
+
+    // Sets newGame = true if all the squares are empty (new game)
+    // Otherwise newGame = false
+    for (var i = 0; i < children.length; i++) {
+      if (children[i].innerText != '') {
+        newGame = false;
+        break;
+      } else {
+        newGame = true;
+      }
+    }
+  });
+
+  // Event listener to place 'X' and 'O'
+  window.addEventListener('mouseup', function(event) {
     const square = event.target;
 
     if (square.classList.contains('square') && (square.innerText === '')) {
 
-        if (oChecker === false){
+        if ((oChecker === false) || (newGame === true)) {
           square.innerText = 'X';
           oChecker = true;
-          test = xoChecker();
+          xoChecker();
         } else {
           square.innerText = 'O';
           oChecker = false;
-          test = xoChecker();
+          xoChecker();
         }
     }
   });
