4d64d9fdcee57eb51adcab104427ce7c6cf24e01
Ben <ben.attali8@gmail.com>
2019-10-21 14:56:17 +0300

    small fixes

--- assets/javascript/xoChecker.js
+++ assets/javascript/xoChecker.js
@@ -24,7 +24,7 @@ const xoChecker = () => {
     $.when().then(function() {
       Swal.fire({
         type: 'info',
-        title: 'Draw...',
+        title: 'Draw',
         confirmButtonText: 'Uncool'
       })
       resetBoard();
